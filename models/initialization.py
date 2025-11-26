5f78613b0f5e5f339306d06bfbbf73b6a59b7bb9
diogosoares22 <diogo.sousa.soares2000@gmail.com>
2025-11-26 23:44:18 +0100

    Fixed initialization to use kaiming instead of xavier

--- models/initialization.py
+++ models/initialization.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 
-def initialize_weights(model, gain=1):
+def initialize_weights(model, gain=sqrt(5)):
     model.apply(lambda m: weights_init(m, gain))
 
-def weights_init(m, gain=1):
+def weights_init(m, gain=sqrt(5)):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-        torch.nn.init.xavier_uniform_(m.weight.data, gain=gain)    
+        torch.nn.init.kaiming_uniform_(m.weight.data, a=gain)    
     
